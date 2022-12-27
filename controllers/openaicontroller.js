e8e0116d097bf8277203989231a0cbac15c8d653
baibhav <baibhavtiwari37@gmail.com>
2022-12-27 22:14:42 +0530

    backend completed

--- controllers/openaicontroller.js
+++ controllers/openaicontroller.js
@@ -1,6 +1,39 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 const generateImage = async (req, res) => {
-    res.status(200).json({
-        success: true,
-    });
-}
+    const { prompt, size } = req.body;
+    const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+
+
+    try {
+        const response = await openai.createImage({
+            prompt,
+            n: 1,
+            size: imageSize,
+        });
+        const imageurl = response.data.data[0].url
+
+        res.status(200).json({
+            success: true,
+            data: imageurl,
+        });
+    } catch (err) {
+        if (err.response) {
+            console.log(err.response.status);
+            console.log(err.response.data);
+        } else {
+            console.log(err.message);
+        }
+        res.status(400).json({
+            success: false,
+            error: 'image could not be created'
+        });
+
+    }
+};
 module.exports = { generateImage }
--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 const app = express();
+// enabling body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
 app.use('/openai', require('./routes/openaiRoutes'));
 
 app.listen(port, () => console.log(`server started on port ${port}`));
