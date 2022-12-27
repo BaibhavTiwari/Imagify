b32e015229e848676a5232279ececafd8bcb32b8
baibhav <baibhavtiwari37@gmail.com>
2022-12-27 21:15:18 +0530

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => console.log("server started on port ${port}"));
