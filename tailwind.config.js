b4b500690a77b28c69c659e7fa5e50e63399c29e
baibhav <baibhavtiwari37@gmail.com>
2022-12-30 12:22:25 +0530

    added tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: [
-    // ...
-    require('tailwindcss'),
-    require('autoprefixer'),
-    // ...
-  ]
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
